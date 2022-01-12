b23b68b0727df580c137664a288f3d35fca2daaf
jaredmartin5 <jaredm776@cox.net>
2022-01-12 17:20:43 -0600

    made 404 page

--- TodoList/ClientApp/src/App.js
+++ TodoList/ClientApp/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import { Layout } from './components/Layout';
 import { CurrentTasks } from './components/CurrentTasks';
 import { CompletedTasks } from './components/CompletedTasks';
+import PageNotFound from './components/PageNotFound';
 import './custom.css'
 
 function App() {
     return (
         <div className="App">
             <Layout>
-                <Route exact path='/' component={CurrentTasks} />
-                <Route path='/completed' component={CompletedTasks} />
+                <Switch>
+                    <Route exact path='/' component={CurrentTasks} />
+                    <Route path='/completed' component={CompletedTasks} />
+                    <Route path='*' component={PageNotFound} />
+                </Switch>
             </Layout>
         </div>
     );
---
+++ TodoList/ClientApp/src/components/PageNotFound.js
@@ -0,0 +1,12 @@
+﻿import { Link } from "react-router-dom";
+
+function PageNotFound()  {
+    return (
+        <>
+            <h2> The page cannot be found! </h2>
+            <Link to="/"> Back to home! </Link>
+        </>
+    );
+};
+
+export default PageNotFound;
