35fd65df47ce8eee8cb071cb8fe173f252f23d2e
jaredmartin5 <jaredm776@cox.net>
2022-01-12 14:04:46 -0600

    title and description required

--- TodoList/Models/TaskItem.cs
+++ TodoList/Models/TaskItem.cs
@@ -1,11 +1,14 @@
-﻿namespace TodoList.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Models
 {
     public class TaskItem
     {
         public int ID { get; set; }
         public int UserId { get; set; }
-        //public User? User { get; set; }
+        [Required]
         public string? Title { get; set; }
+        [Required]
         public string? Description { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? CompletedDate { get; set; }
